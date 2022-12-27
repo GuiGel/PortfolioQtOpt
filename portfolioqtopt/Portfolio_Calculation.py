b6b51f9522be1a8597f90316c6c909093750f23d
ggelabert <ggelabert@serikat.es>
2022-12-27 15:35:34 +0100

    :sparkles: Add SolverTypes enum

--- portfolioqtopt/Portfolio_Calculation.py
+++ portfolioqtopt/Portfolio_Calculation.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from enum import Enum, unique
+from typing import List, Optional
 
 from .dwave_solver import DWaveSolver
 from .PortfolioSelection import PortfolioSelection
@@ -7,34 +9,39 @@ from .QuboBuilder import QUBO
 from .reader import read_welzia_stocks_file
 
 
+@unique
+class SolverTypes(Enum):
+    Clique_Embedding = "Clique_Embedding"
+    Find_Embedding = "Find_Embedding"
+    hybrid_solver = "hybrid_solver"
+    SA = "SA"
+    exact = "exact"
+
+
 def Portfolio_Calculation(
-    API: str,  # clave acceso leap
+    api_token: str,  # clave acceso leap
     slices_arg: int,
     file_name: str,  # A path
     sheet: str,  # sheet name in excel file
     fondos: int,
-    theta_one_arg=0.9,
-    theta_two_arg=0.4,
-    theta_three_arg=0.1,
-    lista_fondos_invertidos=[],
+    theta_one_arg: float = 0.9,
+    theta_two_arg: float = 0.4,
+    theta_three_arg: float = 0.1,
+    lista_fondos_invertidos: Optional[List[str]] = None,
+    solver_type: SolverTypes = SolverTypes.hybrid_solver,
 ):
 
-    # •	Número de fondos (45): el número de fondos que componen el universo. Es decir, los assets donde se puede llevar a cabo una inversión. En el fichero que os enviamos en este mail, por ejemplo, este número es de 45.
+    # Número de fondos (45): el número de fondos que componen el universo.
+    # Es decir, los assets donde se puede llevar a cabo una inversión.
+    # En el fichero que os enviamos en este mail, por ejemplo, este número es de 45.
 
-    ######### Los slices son las proporciones con las que vamos a poder jugar con nuestras acciones #########
+    # Los slices son las proporciones con las que vamos a poder jugar con
+    # nuestras acciones
     slice_nums = {1: 1, 2: 2, 4: 4, 5: 5, 6: 6, 8: 8, 10: 10}
     slices = slice_nums[slices_arg]
 
-    ######### Elegimos el tipo de embedding #########
-    # TODO Seria interesante poner esto en una enumeration
-    solver_types = {
-        1: "Clique_Embedding",
-        2: "Find_Embedding",
-        3: "hybrid_solver",
-        4: "SA",
-        5: "exact",
-    }
-    choose_solver = solver_types[3]
+    # Elegimos el tipo de embedding.
+    choose_solver = solver_type.value
 
     ######### Escogemos el chain strength #########
     chain_strengths = {0.5: 0.5, 0.75: 0.75, 1: 1, 1.25: 1.25, 2: 2}
@@ -68,13 +75,11 @@ def Portfolio_Calculation(
     # AQUI VAMOS A FILTRAR, EN CASO DE SER NECESARIO, EL DATASET
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-    if len(lista_fondos_invertidos) == 0:
+    if lista_fondos_invertidos is None:
         price_data_df = df_price_data[:days]
     else:
         price_data_df = df_price_data[lista_fondos_invertidos]
     price_data = price_data_df.values[:, :assets]
-
-    # np.testing.assert_equal(df_price_data.to_numpy(), df.to_numpy())
 
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # OBTENEMOS LOS VALORES QUE VAN A COMPONER LA MATRIZ QUBO
@@ -100,7 +105,13 @@ def Portfolio_Calculation(
 
     # Configuramos el solver
     dwave_solve = DWaveSolver(
-        qubo_matrix, qubo_dict, runs, chain_strength, anneal_time, choose_solver, API
+        qubo_matrix,
+        qubo_dict,
+        runs,
+        chain_strength,
+        anneal_time,
+        choose_solver,
+        api_token,
     )
 
     # Resolvemos el problema
@@ -112,3 +123,7 @@ def Portfolio_Calculation(
     ) = dwave_solve.solve_DWAVE_Advantadge_QUBO()
 
     return dwave_raw_array, portfolio_selection, new_header, price_data
+
+
+if __name__ == "__main__":
+    print(SolverType[3])

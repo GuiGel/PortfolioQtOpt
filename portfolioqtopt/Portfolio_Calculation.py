267994a1e79127d3339954fdae8747c327a467ac
ggelabert <ggelabert@serikat.es>
2022-12-27 15:58:59 +0100

    :construction: Update num slices

--- portfolioqtopt/Portfolio_Calculation.py
+++ portfolioqtopt/Portfolio_Calculation.py
@@ -20,16 +20,38 @@ class SolverTypes(Enum):
 
 def Portfolio_Calculation(
     api_token: str,  # clave acceso leap
-    slices_arg: int,
+    num_slices: int,
     file_name: str,  # A path
     sheet: str,  # sheet name in excel file
     fondos: int,
-    theta_one_arg: float = 0.9,
-    theta_two_arg: float = 0.4,
-    theta_three_arg: float = 0.1,
+    theta1: float = 0.9,
+    theta2: float = 0.4,
+    theta3: float = 0.1,
     lista_fondos_invertidos: Optional[List[str]] = None,
     solver_type: SolverTypes = SolverTypes.hybrid_solver,
 ):
+    """Compute the portfolio.
+
+    theta1, 2, and 3 are the penalty parameters of the Hamiltonian.
+
+    Args:
+        api_token (str): _description_
+        num_slices (int): Choose the number of slices. Must be >= 0.
+        file_name (str): Name of the welzia data file.
+        theta1 (float, optional): La variable asociada al retorno.
+            Defaults to 0.9.
+        theta2 (float, optional): La variable asociada a que se cumpla la
+            restricción del budget. Defaults to 0.4.
+        theta3 (float, optional): La variable asociada al riesgo.
+            Defaults to 0.1.
+        lista_fondos_invertidos (Optional[List[str]], optional): Name of some
+            specific fund to select. Defaults to None.
+        solver_type (SolverTypes, optional): Choose one type of solvers.
+            Defaults to SolverTypes.hybrid_solver.
+
+    Returns:
+        _type_: _description_
+    """
 
     # Número de fondos (45): el número de fondos que componen el universo.
     # Es decir, los assets donde se puede llevar a cabo una inversión.
@@ -37,30 +59,23 @@ def Portfolio_Calculation(
 
     # Los slices son las proporciones con las que vamos a poder jugar con
     # nuestras acciones
-    slice_nums = {1: 1, 2: 2, 4: 4, 5: 5, 6: 6, 8: 8, 10: 10}
-    slices = slice_nums[slices_arg]
+    assert num_slices >= 1
+    slices_num = num_slices * 2  # NOTE: Is the num_slices alway pair?
 
     # Elegimos el tipo de embedding.
     choose_solver = solver_type.value
 
-    ######### Escogemos el chain strength #########
+    # Escogemos el chain strength
     chain_strengths = {0.5: 0.5, 0.75: 0.75, 1: 1, 1.25: 1.25, 2: 2}
     chain_strength = chain_strengths[1]
 
-    ######### Elegimos el numero de runs #########
+    # Elegimos el numero de runs
     number_runs = {10: 10, 500: 500, 1000: 1000, 2000: 2000, 10000: 10000}
     runs = 10000
 
-    ######### Elegimos el annealing time #########
+    # Elegimos el annealing time
     anneal_time_dict = {1: "1", 5: "5", 100: "100", 250: "250", 500: "500", 999: "999"}
     anneal_time = [500]
-
-    ######### Elegimos los parámetros de penalización del hamiltoniano* #########
-    theta_one = theta_one_arg  # esta es la variable asociada al retorno
-    theta_two = theta_two_arg  # esta es la variable asociada a que se cumpla la restricción del budget
-    theta_three = theta_three_arg  # esta es la variable asociada al riesgo
-
-    # print ('weights: (theta_one, theta_two, theta_three)', theta_one, theta_two, theta_three)
 
     ######### Fijamos el número de días y el numero de fondos que vamos a considerar del dataset completo #########
     days = 8000
@@ -85,7 +100,7 @@ def Portfolio_Calculation(
     # OBTENEMOS LOS VALORES QUE VAN A COMPONER LA MATRIZ QUBO
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     portfolio_selection = PortfolioSelection(
-        theta_one, theta_two, theta_three, price_data, slices
+        theta1, theta2, theta3, price_data, slices_num
     )
 
     # qi son los valores de la diagonal
@@ -123,7 +138,3 @@ def Portfolio_Calculation(
     ) = dwave_solve.solve_DWAVE_Advantadge_QUBO()
 
     return dwave_raw_array, portfolio_selection, new_header, price_data
-
-
-if __name__ == "__main__":
-    print(SolverType[3])

8a68acd6e677f4c4e6f82f1578db92ebbe82ca36
ggelabert <ggelabert@serikat.es>
2022-12-27 23:14:26 +0100

    :art: Update structure of code / comment

--- portfolioqtopt/PortfolioSelection.py
+++ portfolioqtopt/PortfolioSelection.py
@@ -16,10 +16,10 @@ class PortfolioSelection:
     """The PortfolioSelection class.
 
     Attributes:
-        theta_one (float): The weight we give to return.
-        theta_two (float): The weight we give to the penalty, to the constraint of
+        theta1 (float): The weight we give to return.
+        theta2 (float): The weight we give to the penalty, to the constraint of
             not exceeding the budget.
-        theta_three (float): The weight we give to covariance, i.e. to diversity.
+        theta3 (float): The weight we give to covariance, i.e. to diversity.
         price_data (npt.NDArray[np.float64]): At this point in the execution, prices
             are the values of the funds in raw format, without normalizing.
             Shape (m, n) where m is the historical depth of the data and
@@ -32,19 +32,19 @@ class PortfolioSelection:
 
     def __init__(
         self,
-        theta_one: float,
-        theta_two: float,
-        theta_three: float,
+        theta1: float,
+        theta2: float,
+        theta3: float,
         price_data: npt.NDArray[np.float64],
         num_slices: int,
     ) -> None:
         """Initialized the ``PortfolioSelection`` class.
 
         Args:
-            theta_one (float): The weight we give to return.
-            theta_two (float): The weight we give to the penalty, to the constraint of
+            theta1 (float): The weight we give to return.
+            theta2 (float): The weight we give to the penalty, to the constraint of
                 not exceeding the budget.
-            theta_three (float): The weight we give to covariance, i.e. to diversity.
+            theta3 (float): The weight we give to covariance, i.e. to diversity.
             price_data (npt.NDArray[np.float64]): At this point in the execution, prices
                 are the values of the funds in raw format, without normalizing.
                 Shape (m, n) where m is the historical depth of the data and
@@ -53,82 +53,74 @@ class PortfolioSelection:
                 give to each fund. That is, the amount of the budget that we will be
                 able to invest. For example, a 0.5, a 0.25, a 0.125...
         """
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        # OBTENEMOS LOS VALORES DE INPUT
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # >>>>>>>>>>>>
+        # INPUT VALUES
+        # >>>>>>>>>>>>
 
-        self.theta_one = theta_one
-        self.theta_two = theta_two
-        self.theta_three = theta_three
+        self.theta1 = theta1
+        self.theta2 = theta2
+        self.theta3 = theta3
         self.price_data = price_data
         self.num_slices = num_slices
 
-        self.b = 1.0  # Este es el budget, que es igual a 1 en todos los casos
+        self.b = 1.0  # This is the budget, which is equal to 1 in all cases.
 
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        # HACEMOS LA EXPANSION DE LOS PRECIOS EN FUNCIÓN DE LAS SLIDES
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # WE MAKE THE EXPANSION OF THE PRICES ACCORDING TO THE SLIDES
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-        # En este punto es en el que se normalizan los precios de cada fondo,
-        # utilizando como base el ultimo valor registrado. A raíz de ese valor
-        # y en función de las slides, se va componiendo el resto de precios
+        # It is at this point that the prices of each fund are normalized, using the
+        # last value recorded as a basis. Based on this value and depending on the
+        # slides, the rest of the prices are composed as follows.
+
         expand = ExpandPriceData(self.b, self.num_slices, self.price_data)
 
-        # Se substituye los precios en formato raw por los precios en formato
-        # normalizado.
-        self.price_data = expand.price_data_expanded
+        # Prices in raw format are replaced by prices in standardized format.
+
+        self.price_data = expand.price_data_expanded  # TODO: Change name to standardized_price
         self.price_data_reversed = expand.price_data_expanded_reversed
 
-        # Los precios posibles, esto realmente es una lista de la proporción
-        # del budget que puedes invertir para cada uno de los fondos.
-        # Por ejemplo: 1.0, 0.5, 0.25, 0.125
+        # Possible prices, this is actually a list of the proportion of the budget you
+        # can invest for each of the funds. For example: 1.0, 0.5, 0.25, 0.125
         # NOTE: We talk about the final possible prices
+    
         self.prices = self.price_data[-1, :]
 
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        # OBTENEMOS EL EXPECTED RETURN
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # COMPUTE THE EXPECTED RETURN
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-        # Obtenemos el retorno esperado utilizando los precios como base
+        # We obtain the expected return using the prices as a basis.
 
-        # Compute the mean of the daily returns
+        # Compute the mean of the daily returns.
+
         self.expected_returns = get_expected_returns(self.price_data)
 
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        # OBTENEMOS EL EXPECTED RETURN
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
         # Obtenemos los valores asociados al riesgo, es decir, la covariance
-        QUBO_covariance = get_prices_covariance(self.price_data)
+        qubo_covariance = get_prices_covariance(self.price_data)
 
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        # CONFORMACIÓN DE LOS VALORES DEL QUBO
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # SHAPING THE VALUES OF THE QUBO
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-        # Generamos una matriz diagonal con los retornos, esta matriz la
-        # usaremos luego con el valor de theta_one
-        QUBO_returns = np.diag(self.expected_returns)
+        # We generate a diagonal matrix with the returns, this matrix will be used later
+        # with the value of theta1.
+        qubo_returns = np.diag(self.expected_returns)
 
-        # Generamos una matriz diagonal con los precios posibles * 2.
-        # Esto se relacionara con los returns
-        QUBO_prices_linear = 2.0 * self.b * np.diag(self.prices)  # (m, n)
+        # We generate a diagonal matrix with the possible prices * 2. This will be
+        # related to the returns.
+        qubo_prices_linear = 2.0 * self.b * np.diag(self.prices)  # (m, n)
 
-        # Generamos una matriz simétrica también relacionada con los precios
-        # posibles. Esto se relacionara con la diversidad.
-        QUBO_prices_quadratic = np.outer(self.prices, self.prices)  # (m, n)
+        # We generate a symmetric matrix also related to the possible prices. This will
+        # be related to diversity.
+        qubo_prices_quadratic = np.outer(self.prices, self.prices)  # (m, n)
 
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        # FORMACIÓN DEFINITIVA DEL QUBO, CON LOS VALORES DE BIAS Y PENALIZACIÓN INCLUIDOS
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # FINAL QUBO FORMATION, WITH BIAS AND PENALTY VALUES INCLUDED
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-        # Primero conformamos los valores de la diagonal, relacionados con el
-        # return y los precios.
-        self.qi = -(self.theta_one * QUBO_returns) - (
-            self.theta_two * QUBO_prices_linear
-        )
+        # We form the diagonal values, related to return and prices.
+        self.qi = -self.theta1 * qubo_returns - self.theta2 * qubo_prices_linear
 
-        # Ahora conformamos los valores cuadráticos, relacionados con la
-        # diversidad.
-        self.qij = (self.theta_two * QUBO_prices_quadratic) + (
-            self.theta_three * QUBO_covariance
-        )
+        # We now form the quadratic values, related to diversity.
+        self.qij = self.theta2 * qubo_prices_quadratic + self.theta3 * qubo_covariance

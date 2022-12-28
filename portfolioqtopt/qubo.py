d9b8cc9d08e7403e0edfdc323ab9b2887bc23978
ggelabert <ggelabert@serikat.es>
2022-12-28 14:13:59 +0100

    :rotating_light: Update linting warning

--- portfolioqtopt/qubo.py
+++ portfolioqtopt/qubo.py
@@ -41,7 +41,7 @@ class QUBO:
 
 from typing import Dict, NamedTuple, Tuple
 
-QuboDict = Dict[Tuple[int, int], npt.NDArray[np.float64]]
+QuboDict = Dict[Tuple[int, int], np.floating]
 
 
 class Qubo(NamedTuple):
@@ -73,7 +73,7 @@ def get_qubo_dict(q: npt.NDArray[np.float64]) -> QuboDict:
             corresponding matrix value q[i, j].
     """
     n = len(q)
-    qubo_dict: QuboDict = {z: q[z] for z in it.product(*(range(n),) * 2)}
+    qubo_dict: QuboDict = {(i, j): q[i, j] for i in range(n) for j in range(n)}
     return qubo_dict
 
 

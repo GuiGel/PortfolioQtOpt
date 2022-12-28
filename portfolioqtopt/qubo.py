9cbbd031109abf5d717cfc9771461cd93d05b348
ggelabert <ggelabert@serikat.es>
2022-12-28 08:26:28 +0100

    :construction: Add get_qubo function

--- portfolioqtopt/qubo.py
+++ portfolioqtopt/qubo.py
@@ -15,13 +15,10 @@ class QUBO:
     def __init__(
         self, qi: npt.NDArray[np.float64], qij: npt.NDArray[np.float64]
     ) -> None:
-        self.qi = qi
-        self.qij = qij
-
         # Obtenemos las dimensiones del problema,
         # m = la profundidad historica de los datos
         # n = el numero de fondos * el numero de slices
-        m, n = self.qij.shape
+        m, n = qij.shape
 
         # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
         # GENERAMOS EL QUBO
@@ -40,3 +37,45 @@ class QUBO:
         # Generamos el diccionario, que es lo que vamos a emplear para
         # resolver el problema en DWAVE.
         self.qubo_dict = {z: self.qubo[z] for z in it.product(range(n), range(n))}
+
+
+from typing import Dict, NamedTuple, Tuple
+
+QuboDict = Dict[Tuple[int, int], npt.NDArray[np.float64]]
+
+
+class Qubo(NamedTuple):
+    matrix: npt.NDArray[np.float64]
+    dictionary: QuboDict
+
+
+def get_qubo(qi: npt.NDArray[np.float64], qij: npt.NDArray[np.float64]) -> Qubo:
+    """Compute the qubo matrix and the corresponding dictionary.
+
+    Args:
+        qi (npt.NDArray[np.float64]): Diagonal, related to expected returns. Shape
+            (m, n) where m is the historical depth of the data and
+            n = number of funds * number of slices.
+        qij (npt.NDArray[np.float64]): The quadratic part, related to variances.
+
+    Returns:
+        Qubo: _description_
+    """
+    # TODO: Here there is an error. Mat sym
+    _, n = qij.shape
+
+    # >>>>>>>>>>>>>>
+    # COMPUTE QUBO
+    # >>>>>>>>>>>>>>
+
+    qubo = qi + qij
+
+    # At first the matrix is complete, so with this method only the upper part of this
+    # matrix is obtained.
+
+    qubo_matrix = get_upper_triangular(qubo)
+
+    # We generate the dictionary, which we will use to solve the problem in DWAVE.
+    qubo_dict: QuboDict = {z: qubo_matrix[z] for z in it.product(*(range(n),) * 2)}
+
+    return Qubo(qubo_matrix, qubo_dict)

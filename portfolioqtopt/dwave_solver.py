952de8b709e728aff534e7113bdf9472037bd2fc
ggelabert <ggelabert@serikat.es>
2022-12-28 12:54:56 +0100

    :construction: Add url as cls attribute

--- portfolioqtopt/dwave_solver.py
+++ portfolioqtopt/dwave_solver.py
@@ -6,6 +6,9 @@ from dwave.system.samplers import \
 
 
 class DWaveSolver(object):
+
+    _URL = "https://na-west-1.cloud.dwavesys.com/sapi/v2/"
+
     def __init__(self, qubo, qubo_dict, runs, chainstrength, anneal_time, solver, API):
 
         # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -19,8 +22,6 @@ class DWaveSolver(object):
         self.solver = solver
         # self.sapi_token = 'DEV-d9751cb50bc095c993f55b3255f728d5b2793c36'
         self.sapi_token = API
-        # self.url = 'https://eu-central-1.cloud.dwavesys.com/sapi/v2/'
-        self.url = "https://na-west-1.cloud.dwavesys.com/sapi/v2/"
 
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # RESOLVEMOS EL PROBLEMA EMPLEANDO DWAVE, LOS PARAMETROS ARRIBA INDICADOS
@@ -29,7 +30,7 @@ class DWaveSolver(object):
     def solve_DWAVE_Advantadge_QUBO(self):
 
         if self.solver == "hybrid_solver":
-            sampler = LeapHybridSampler(token=self.sapi_token, endpoint=self.url)
+            sampler = LeapHybridSampler(token=self.sapi_token, endpoint=self._URL)
 
         ######### Resolvemos el problema y obtenemos la solución #########
         if self.solver == "hybrid_solver" or self.solver == "exact":

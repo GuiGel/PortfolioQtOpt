ca5bc4231d96c994c168be1a1d4199fddcc58494
ggelabert <ggelabert@serikat.es>
2022-12-28 14:15:20 +0100

    :construction: Add solve_dwave_advantage_cubo

--- portfolioqtopt/dwave_solver.py
+++ portfolioqtopt/dwave_solver.py
@@ -1,46 +1,31 @@
-# coding=utf-8
+from enum import Enum, unique
 
+from dimod.sampleset import SampleSet
 from dwave.system import LeapHybridSampler
-from dwave.system.samplers import \
-    DWaveSampler  # Library to interact with the QPU
+
+from portfolioqtopt.qubo import Qubo
 
 
-class DWaveSolver(object):
+@unique
+class SolverTypes(Enum):
+    Clique_Embedding = "Clique_Embedding"
+    Find_Embedding = "Find_Embedding"
+    hybrid_solver = "hybrid_solver"
+    SA = "SA"
+    exact = "exact"
 
+
+def solve_dwave_advantage_cubo(
+    qubo: Qubo, solver: SolverTypes, api_token: str
+) -> SampleSet:
+
+    # api_token = 'DEV-d9751cb50bc095c993f55b3255f728d5b2793c36'
     _URL = "https://na-west-1.cloud.dwavesys.com/sapi/v2/"
 
-    def __init__(self, qubo, qubo_dict, chainstrength, solver, api_token):
-
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        # OBTENEMOS LOS VALORES PARA RESOLVER EL PROBLEMA A TRAVES DE DWAVE
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        self.chainstrength = chainstrength
-        self.qubo = qubo
-        self.qubo_dict = qubo_dict
-        self.solver = solver
-        self.api_token = api_token  # 'DEV-d9751cb50bc095c993f55b3255f728d5b2793c36'
-
-    # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    # RESOLVEMOS EL PROBLEMA EMPLEANDO DWAVE, LOS PARÁMETROS ARRIBA INDICADOS
-    # Y EL QUBO CON EL QUE HEMOS INICIALIZADO LA CLASE
-    # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    def solve_DWAVE_Advantadge_QUBO(self):
-
-        if self.solver == "hybrid_solver":
-            sampler = LeapHybridSampler(token=self.api_token, endpoint=self._URL)
-
-        ######### Resolvemos el problema y obtenemos la solución #########
-        if self.solver == "hybrid_solver" or self.solver == "exact":
-            self.dwave_return = sampler.sample_qubo(self.qubo_dict)
-
-        self.dwave_raw_array = self.dwave_return.record.sample
-        self.num_occurrences = self.dwave_return.record.num_occurrences
-        self.energies = self.dwave_return.record.energy
-
-        ######### Devolvemos la solucion, ocurrencias y energias #########
-        return (
-            self.dwave_return,
-            self.dwave_raw_array,
-            self.num_occurrences,
-            self.energies,
-        )
+    if solver == "hybrid_solver":
+        sampler = LeapHybridSampler(token=api_token, endpoint=_URL)
+        sampleset = sampler.sample_qubo(qubo.dictionary)  # type: ignore
+        # mypy complains --> https://github.com/python/mypy/issues/2412
+        return sampleset
+    else:
+        raise ValueError(f"Bad solver. Solver must be hybrid_solver.")

98269414ed779e99d90a18df486814e492967e28
ggelabert <ggelabert@serikat.es>
2023-01-10 13:46:55 +0100

    :construction: Update test _opt_step

--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -5,9 +5,10 @@ from unittest.mock import PropertyMock, patch
 import numpy as np
 import numpy.typing as npt
 import pytest
+from loguru import logger
 
 from portfolioqtopt.markovitz_portfolio import Selection
-from portfolioqtopt.optimizer import Optimizer, SolverTypes
+from portfolioqtopt.optimizer import Indexes, Optimizer, SolverTypes
 from portfolioqtopt.qubo import QuboFactory
 
 
@@ -15,16 +16,14 @@ from portfolioqtopt.qubo import QuboFactory
 def qubo_factory() -> QuboFactory:
     prices = np.array(
         [
-            [
-                100,
-                104,
-                102,
-            ],
-            [10, 10.2, 10.4],
+            [100, 104, 102, 104, 100],
+            [10, 10.2, 10.4, 10.5, 10.4],
+            [50, 51, 52, 52.5, 52],
+            [1.0, 1.02, 1.04, 1.05, 1.04],
         ],
         dtype=np.float64,
     ).T
-    selection = Selection(prices, 2, 1.0)
+    selection = Selection(prices, 6, 1.0)
     return QuboFactory(selection, 0.1, 0.2, 0.3)
 
 
@@ -56,6 +55,36 @@ class TestOptimizer:
         ) as mocked_interpreter_selected_indexes:
             mocked_interpreter_selected_indexes.side_effect = selected_indexes
             assert optimizer.reduce_dimension(steps) == expected_counter
+
+    @pytest.mark.parametrize("indexes, sharpe_ratio", [([0, 1, 2, 3], 1.0)])
+    def test_opt_step(
+        self, qubo_factory, indexes: Indexes, sharpe_ratio: float
+    ) -> None:
+        # In the first test we have initial indexes of [0, 1, 2, 3] and a sharpe ratio of 1.0
+        # Then we say that the new selected indexes are [0, 1, 3] and the new sharpe ratio is bigger than that the initial one.
+        # So the result of Optimizer._opt_step must be [0, 1, 3] and the corresponding interpreter.
+
+        optimizer = Optimizer(qubo_factory, "", SolverTypes.hybrid_solver)
+        np.set_printoptions(precision=1)
+        logger.info(optimizer.qubo_factory.qubo.matrix.shape)
+
+        with patch(
+            "portfolioqtopt.optimizer.Interpret.selected_indexes",
+            new_callable=PropertyMock,
+        ) as mocked_interpreter_selected_indexes, patch(
+            "portfolioqtopt.optimizer.Interpret.sharpe_ratio",
+            new_callable=PropertyMock,
+        ) as mocked_interpreter_sharpe_ratio:
+
+            # gives a value to the properties of the simulated interpreter
+            mocked_interpreter_selected_indexes.return_value = np.array([0, 1, 3])
+            mocked_interpreter_sharpe_ratio.return_value = 2.0
+
+            # test the function with the mock properties
+            indexes, interpret = optimizer._opt_step(np.array(indexes), sharpe_ratio)
+            logger.info(f"Return indexes: {indexes}")
+            assert indexes.tolist() == [0, 1, 3]
+            assert interpret is not None
 
 
 """def test_reduce_dimension():

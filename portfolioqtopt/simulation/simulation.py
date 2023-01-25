66f2382505a9e4dbcf96e4292093ee7bc6dde973
ggelabert <ggelabert@serikat.es>
2023-01-25 17:35:13 +0100

    :books: Update draft send to eneko

--- portfolioqtopt/simulation/simulation.py
+++ portfolioqtopt/simulation/simulation.py
@@ -35,20 +35,17 @@ to simulate the future prices :math:`P^{s}\\in\\large{R}^{N^{s}\\times{m}}` of \
     the time period of the simulation.
 
 
-How does it works ?
--------------------
-
 1. Generate daily returns with a given covariance
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 To do this, we first generate a matrix :math:`M\\in\\large{R}^{N^{s}-1\\times{m} }` \
 composed of random values drawn from the standard normal distribution from which we \
-will generate the desired daily returns from :math:`M`.
+will generate some simulated daily returns :math:`Q` such that :math:`Cov(Q)=C^{h}`.
 
 The covariance matrix :math:`Cov(M)` is a symmetric positive definite real-valued \
-matrix which is not exactly the identity matrix :math:`I`, we use the Choleski \
-decomposition which tells us that there is a unique matrix \
-:math:`L\\in\\large{R}^{m\times{m}}` upper triangular with real and positive diagonal \
+matrix which is not exactly the identity matrix :math:`I`. We use the Choleski \
+decomposition of :math:`Cov(M)` which tells us that there is a unique matrix \
+:math:`L\\in\\large{R}^{m\\times{m}}` upper triangular with real and positive diagonal \
 entries such that :math:`Cov(M)=C^{M}=L^{T}L`.
 
 We can do the same for :math:`C^{h}` and write that there is a unique upper triangular \
@@ -57,8 +54,8 @@ matrix :math:`L^{h}\\in\\large{R}^{m\\times{m}}` such that :math:`C^{h}=L_{h}^{T
 Now let's show that if the matrices :math:`L` and :math:`L^{h}` are invertible, then \
 by setting :math:`Q=ML^{-1}L_{h}` it follows that :math:`Cov(Q)=C^{h}` .
 
-To do this let's use the fact :math:`\\mathbb{E}(MA)=\\mathbb{E}(M)A` and return to \
-the definition of covariance: TODO Demostrate it is needed.
+To do this let's use the fact that :math:`\\mathbb{E}(MA)=\\mathbb{E}(M)A` by \
+linearity of the expectation and the definition of covariance to write:
 
 .. math:: 
     Cov(Q) &= \\mathbb{E}[(MA - \\mathbb(MA))^{T}(MA - \\mathbb(MA)))]
@@ -83,16 +80,13 @@ or we have seen that by definition :math:`Cov(M)=C^{M}=L^{T}L` so we have \
 2. Generate daily returns with the given expected return
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-We have a matrix of daily returns :math:`Q` that have the same covariance \
-matrix :math:`C^{h}` as the one of the daily return of our historical prices. But the \
-vector of anual expected returns :math:`Er^{q}` that correspond to these simulated \
-daily returns is not the same as the original one :math:`Er`.
+We have a matrix of daily returns :math:`Q` that have the same covariance matrix :math:`C^{h}` as our historical daily price returns. But the vector of expected annual returns :math:`Er^{q}` that corresponds to these simulated daily returns is not the same as the original vector :math:`Er`.
 
 By using the fact that, if :math:`c \\in R^{N^{s}-1}` a constant vector then \
-:math:`\\mathbb{E}[Q+c^T]=\\mathbb{E}[Er^{s}]`, we can demonstrate that we still have \
-:math:`Cov(Q + c^T)=C^{h}`. (TODO Make the demo if needed)
+:math:`\\mathbb{E}[Q+c^T]=\\mathbb{E}[Q]`, we can demonstrate that we still have \
+:math:`Cov(Q + c^T)=C^{h}`. (TODO Make the demo if needed).
 
-So our goal is to found :math:`c` such that :math:`Er^{q} + c^T = Er`.
+So our goal is to found :math:`c` such that :math:`Q + c^T = Ed^{s}` .
 
 Given an asset :math:`a_{u}`, with an expected return :math:`r_{u}` between days \
 :math:`0` and :math:`t` we have:
@@ -120,8 +114,8 @@ certain order :math:`n\\geq 1`.
     where the remaining :math:`R_{n}(x)` is a negligible function with respect to \
     :math:`(x-a)^{n}` in the neighbourhood of :math:`a`.
 
-If we apply to the *Taylor theorem* to the logarithm function in 1 we have for all \
-:math:`x > 0`:   
+If we apply the *Taylor theorem* to the logarithm function in 1 we have for all \
+:math:`x > -1`:   
 
 .. math:: ln(1+x)=\\sum_{k=1}^{n} {(-1)^{k-1}\\frac{x^{k}}{k}}+ R_{n}(1+x)
 
@@ -132,7 +126,7 @@ If :math:`x < 1` then the *Taylor-Young* formula stand that we have:
 In our particular case, we know that the daily returns :math:`r_{u, i}` are strictly \
 less than 1 for all :math:`i` and :math:`u`. We can therefore always find a strictly \
 positive integer :math:`n` such that :math:`ln(1+r_{u,i})` is approximated with a great \
-accuracy by is corresponding polynomial *Taylor-Young* approximation or order :math:`n` \
+accuracy by is corresponding polynomial *Taylor-Young* approximation of order :math:`n` \
 . 
 
 .. math::
@@ -141,21 +135,23 @@ accuracy by is corresponding polynomial *Taylor-Young* approximation or order :m
 
 
 So for a given stock :math:`u` with  :math:`N^{s}_{f}` daily returns, and :math:`r_{u, i}` a \
-daily return at day :math:`i`, we can try to found the constant :math:`c_{u}` such \
-that for a simulated daily return :math:`rs_{u,i}` we have:
+    daily return at day :math:`i`, we can try to found the constant :math:`c_{u}` such \
+    that for a simulated daily return :math:`q_{u,i}` we have \
+    :math:`r_{u,i} = q_{u,i} + c_{u}` and:
 
-.. math:: \\sum_{i=1}^{N^{s}_{f}}{ln(1 + rs_{u,i} + c_{u})} = ln(1 + r_{u})
+.. math:: \\sum_{i=1}^{N^{s}_{f}}{ln(1 + q_{u,i} + c_{u})} = ln(1 + r_{u})
 
-To solve this equation we will use the *Taylor-Young* approximation to create the \
-polynomial :math:`P_{u}^{n}(X)` of order :math:`n` such that:
+To find :math:`c_{u}` we will solve an approximation of these equation by replacing \
+    the logarithmic parts with their *Taylor-Young* approximation to create the \
+    polynomial :math:`P_{u}^{n}(X)` of order :math:`n` such that:
 
-.. math:: P_{u}^{n}(X) = \\sum_{i=1}^{N^{s}_{f}} \\sum_{k=1}^n(-1)^{k-1}{\\frac{(rs_{u,i} + X)^{k}}{k}} - ln(1 + r_{u})
+.. math:: P_{u}^{n}(X) = \\sum_{i=1}^{N^{s}_{f}} \\sum_{k=1}^n(-1)^{k-1}{\\frac{(q_{u,i} + X)^{k}}{k}} - ln(1 + r_{u})
 
 
-Let be :math:`c_{n,u}` a root of  :math:`P_{u}^{n}(X)`.  
+Let be :math:`c_{n,u}` a root of :math:`P_{u}^{n}(X)`.  
 
-If :math:`P_{u}^{n}(X)` as at least one real roots such that \
-:math:`| \\underset{1 \\leq i \\leq n}{max}(rs_{u,i}) + c_{n,u} | < 1` we take the  \
+If :math:`P_{u}^{n}(X)` as at least one real root :math:`c_{n,u}` such that \
+:math:`| \\underset{1 \\leq i \\leq n}{max}(q_{u,i}) + c_{n,u} | < 1` we take the  \
 smallest one of these roots :math:`c_{n,u}` and we can observe that:  (TODO demo ?)
 
 .. math:: \\lim_{n \\to +\\infty}(c_{n,u}) = c_{u}
@@ -163,6 +159,12 @@ smallest one of these roots :math:`c_{n,u}` and we can observe that:  (TODO demo
 
 Our algorithm fails if :math:`P_{u}^{n}(X)` as no such root!
 
+So be solving these equation for all the assets :math:`A` we obtain :math:`c` such that \
+we have:
+
+.. math:: 
+
+    Ed^{s} = ML^{-1}L_{h} + c
 """
 from typing import Dict, List, Optional, Tuple
 

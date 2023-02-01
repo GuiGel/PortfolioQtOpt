57da87f98c2301f4dfca2634b685320c743ebeb3
ggelabert <ggelabert@serikat.es>
2023-02-01 10:26:11 +0100

    :art: Update improve module hierarchy

---
+++ portfolioqtopt/optimization/__init__.py
@@ -0,0 +1,4 @@
+from portfolioqtopt.optimization.interpreter import Interpretation
+from portfolioqtopt.optimization.optimization import SolverTypes, optimize
+
+__all__ = ["Interpretation", "optimize", "SolverTypes"]

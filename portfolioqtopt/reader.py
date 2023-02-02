b6541bc21869f5247cb081b2ce53730d9d82fa36
ggelabert <ggelabert@serikat.es>
2023-02-02 08:52:54 +0100

    :construction: Update add new file type for app

--- portfolioqtopt/reader.py
+++ portfolioqtopt/reader.py
@@ -3,10 +3,11 @@ from pathlib import Path
 from typing import Union
 
 import pandas as pd
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 
 def read_welzia_stocks_file(
-    file_path: Union[Path, str],
+    file_path: Union[Path, str, UploadedFile],
     sheet_name: str,
 ) -> pd.DataFrame:
     """Read welzia .xlsx input files as a pandas ``DataFrame``.
@@ -21,8 +22,9 @@ def read_welzia_stocks_file(
     Returns:
         pd.DataFrame: The stock prices read as a pandas ``DataFrame``.
     """
-    if not Path(file_path).exists():
-        raise FileExistsError()
+    if isinstance(file_path, (str, Path)):
+        if not Path(file_path).exists():
+            raise FileExistsError()
 
     df = (
         pd.read_excel(

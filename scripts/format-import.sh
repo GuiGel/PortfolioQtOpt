9348101f0dac887fb37b68c92ddd3aa1f57cf1dd
ggelabert <ggelabert@serikat.es>
2023-02-06 17:30:18 +0100

    :wrench: Add app to formatter scripts

--- scripts/format-import.sh
+++ scripts/format-import.sh
@@ -2,5 +2,5 @@
 set -x
 
 # Sort imports one per line, so autoflake can remove unused imports
-isort --force-single-line-imports portfolioqtopt tests
+isort --force-single-line-imports portfolioqtopt tests application
 sh ./scripts/format.sh

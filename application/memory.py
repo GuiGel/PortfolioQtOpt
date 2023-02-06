79af509af6e7e622d213116b81c8ec3c7cdd767c
ggelabert <ggelabert@serikat.es>
2023-02-06 12:13:31 +0100

    :construction: Update move registry in Item module

--- application/memory.py
+++ application/memory.py
@@ -1,14 +1,31 @@
 from loguru import logger
 from collections import defaultdict
-from typing import DefaultDict, Any, Optional
+from typing import DefaultDict, Any, Optional, List
 from dataclasses import dataclass, field
+import streamlit as st
 
 @dataclass
 class Item:
     order: int = 0
     calls: int = 0
-    val: Optional[Any] = None
+    args: Any = None
+    val: Any = None
 
+
+def registry(name: str, order: int, parameters: List[str]) -> None:
+    logger.debug(f"register_args")
+
+    # Update call number to the form
+    logger.info(f"{memo[name]=}")
+    memo[name].calls += 1
+    memo[name].order = order
+
+    logger.info(f"v1 = {st.session_state.f1_v1}")
+    logger.info(f"v2 = {st.session_state.f1_v2}")
+    logger.info(f"F1 = {st.session_state.keys()}")
+
+    memo[name].args = tuple(map(st.session_state.get, parameters))
+    logger.info(f"{memo[name].args=}")
 
 @dataclass
 class controller:

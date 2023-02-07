f6e773adb120682ce11693c394597676a44beb9a
ggelabert <ggelabert@serikat.es>
2023-02-07 15:25:04 +0100

    :construction: Update merge args creation inside forms

--- portfolioqtopt/application/session_state_.py
+++ portfolioqtopt/application/session_state_.py
@@ -1,12 +1,14 @@
 import os
 import typing as ty
+
 import streamlit as st
 from bokeh.palettes import Turbo256  # type: ignore[import]
 from bokeh.plotting import figure  # type: ignore[import]
 from dotenv import load_dotenv
 from loguru import logger
-from pathlib import Path
 from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+from portfolioqtopt.application.utils import visualize_assets
 from portfolioqtopt.assets import Assets
 from portfolioqtopt.reader import read_welzia_stocks_file
 
@@ -30,7 +32,10 @@ def form_submit_button_callback(key: str) -> None:
         st.session_state[key] += 1
 
 
-def step_0_form(form_key: str, file_uploader_key: str, sheet_name_key: str, button_key: str) -> None:
+def step_0_form(
+    form_key: str, file_uploader_key: str, sheet_name_key: str, button_key: str
+) -> ty.Optional[ty.Tuple[UploadedFile, str]]:
+    # collect value of argument with a form
     logger.info("load data")
 
     with st.form(key=form_key):
@@ -56,14 +61,10 @@ def step_0_form(form_key: str, file_uploader_key: str, sheet_name_key: str, butt
             or st.session_state[sheet_name_key] is None
         ):
             e = ValueError("You must chose a file and a sheet name before submit!")
-            # we remove the press count
             st.session_state[button_key] -= 1
             st.exception(e)
 
-        logger.info(f"{submit=}")
-
-
-def step_0_get_args(file_uploader_key: str, sheet_name_key: str) -> ty.Optional[ty.Tuple[UploadedFile, str]]:    # argument to process step 1
+    # Create the arguments for calling Assets
     if (
         st.session_state.get(file_uploader_key) is not None
         and st.session_state.get(sheet_name_key) is not None
@@ -74,33 +75,10 @@ def step_0_get_args(file_uploader_key: str, sheet_name_key: str) -> ty.Optional[
         return None
 
 
-def step_0_compute(*args: str) -> Assets:
-    df = read_welzia_stocks_file(*args)
-    return Assets(df=df)
-
-
-def visualize_assets(assets: Assets) -> None:
-    logger.info("visualize df")
-    # ----- bokeh visualization
-    p = figure(
-        title="simple line example",
-        x_axis_label="Fechas",
-        y_axis_label="Activos",
-        x_axis_type="datetime",
-        y_axis_type="log",
-        background_fill_color="#fafafa",
-    )
-
-    for i, column in enumerate(assets.df):
-        color = Turbo256[int(256 * i / assets.df.shape[1])]
-        p.line(
-            assets.df.index,
-            assets.df[column],
-            legend_label=column,
-            line_width=1,
-            color=color,
-        )
-    st.bokeh_chart(p, use_container_width=True)
+def step_0_compute(file: UploadedFile, sheet_name: str) -> Assets:
+    df = read_welzia_stocks_file(file, sheet_name)
+    st.session_state["history"] = Assets(df=df)
+    return st.session_state["history"]
 
 
 if __name__ == "__main__":
@@ -109,17 +87,24 @@ if __name__ == "__main__":
 
     args2 = None
 
-    step_0_form("xlsm1", f"xlsm1_file", f"xlsm1_sheet_name", "xlsm1_pressed")
-    args1 = step_0_get_args(f"xlsm1_file", f"xlsm1_sheet_name")
-
-    if st.session_state.get("xlsm1_pressed"):
-
-        step_0_form("xlsm2", f"xlsm2_file", f"xlsm2_sheet_name", "xlsm2_pressed")
-        args2 = step_0_get_args(f"xlsm2_file", f"xlsm2_sheet_name")
+    args1 = step_0_form("xlsm1", f"xlsm1_file", f"xlsm1_sheet_name", "xlsm1_pressed")
 
     if args1 is not None:
-        st.write(args1)
-    if args2 is not None:
-        st.write(args2)
 
+        history = step_0_compute(*args1)
 
+        with st.expander("Raw historical assets"):
+            st.dataframe(data=history.df)
+
+        with st.expander("Plot the historical assets"):
+            visualize_assets(history)
+
+        args2 = step_0_form(
+            "xlsm2", f"xlsm2_file", f"xlsm2_sheet_name", "xlsm2_pressed"
+        )
+
+        if args2 is not None:  # Not useful the get args is enough..
+            assets2 = step_0_compute(*args2)
+
+    st.write(args1)
+    st.write(args2)

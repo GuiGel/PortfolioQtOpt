39d7e776b5d25466008240e178cc88a69521a3bd
ggelabert <ggelabert@serikat.es>
2023-02-07 12:29:38 +0100

    :wrench: Update format script with new app loc

--- scripts/format-import.sh
+++ scripts/format-import.sh
@@ -2,5 +2,5 @@
 set -x
 
 # Sort imports one per line, so autoflake can remove unused imports
-isort --force-single-line-imports portfolioqtopt tests application
+isort --force-single-line-imports portfolioqtopt tests
 sh ./scripts/format.sh
--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place portfolioqtopt tests application --exclude=__init__.py
-black portfolioqtopt tests docs/source/simulation application
-isort portfolioqtopt tests application
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place portfolioqtopt tests --exclude=__init__.py
+black portfolioqtopt tests docs/source/simulation
+isort portfolioqtopt tests

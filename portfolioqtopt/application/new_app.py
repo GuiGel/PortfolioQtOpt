43f8d70e948a144e34ea945189199c1b2f2ad1cf
ggelabert <ggelabert@serikat.es>
2023-02-09 11:04:17 +0100

    :construction: Update app with text

--- portfolioqtopt/application/new_app.py
+++ portfolioqtopt/application/new_app.py
@@ -12,7 +12,8 @@ from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from portfolioqtopt.application.utils import visualize_assets
 from portfolioqtopt.assets import Assets, Scalar
-from portfolioqtopt.optimization import Interpretation, SolverTypes
+from portfolioqtopt.optimization import Interpretation, SolverTypes, optimize
+from portfolioqtopt.optimization.interpreter import Interpretation
 from portfolioqtopt.reader import read_welzia_stocks_file
 from portfolioqtopt.simulation import simulate_assets
 
@@ -50,9 +51,9 @@ def form_submit_button_callback(key: str, order: int) -> None:
 
 def step_0_form() -> Tuple[bool, Optional[UploadedFile], str]:
     with st.sidebar:
-        st.markdown(f"## Parameters for loading historical assets")
+        st.markdown(f"## Load historical assets")
         with st.form(key="xlsm"):
-            with st.expander("Load from xlsm"):
+            with st.expander("from xlsm file"):
 
                 file = st.file_uploader(
                     "Elige un fichero al formato Welzia", type="xlsm"
@@ -72,6 +73,7 @@ def step_0_form() -> Tuple[bool, Optional[UploadedFile], str]:
     if submit and (file is None or sheet_name is None):
         e = ValueError("You must chose a file and a sheet name before submit!")
         st.exception(e)
+        submit = False
 
     return submit, file, sheet_name
 
@@ -85,7 +87,7 @@ def step_1_form(
     assets: Assets,
 ) -> Tuple[bool, int, Optional[Dict[Union[Scalar, Tuple[Hashable, ...]], float]]]:
     with st.sidebar:
-        st.markdown("## Parameters for assets simulation")
+        st.markdown("## Simulate future assets")
         with st.form(key="simulation"):
             with st.expander(
                 "Rellenar las distintas opciones si se necesita o seguir con los "
@@ -93,11 +95,14 @@ def step_1_form(
             ):
                 # ----- select number of days
                 days_number = st.number_input(
-                    "numero de días a simular", value=256, step=1
+                    "Choose a number of days to simulate",
+                    help="Chose a number of days for which a prices must be simulated.",
+                    value=256,
+                    step=1,
                 )
 
                 # ----- select expected return for each asset
-                st.text("Elije el retorno para cada activo:")
+                st.text("Choose an expected returns")
                 expected_returns: Dict[Union[Scalar, Tuple[Hashable, ...]], float] = {}
                 for fund, er in zip(assets.df, assets.anual_returns.tolist()):
                     expected_returns[fund] = st.number_input(
@@ -130,7 +135,7 @@ def step_1_compute(
 def step_2_form() -> Tuple[bool, float, int, float, float, float, int]:
     logger.info("optimize portfolio")
     with st.sidebar:
-        st.markdown("## Parameters for portfolio optimization")
+        st.markdown("## Portfolio Optimization")
         with st.form(key="optimization"):
             with st.expander(
                 "Rellenar las distintas opciones si se necesita o seguir con los "
@@ -172,9 +177,9 @@ def step_2_compute(
     import time
 
     logger.info(f"step 2")
-    with st.spinner("Optimización en curso..."):
+    with st.spinner("Ongoing optimization... Take your time it can last!"):
         time.sleep(2)
-        """ _, interpretation = optimize(
+        _, interpretation = optimize(
             assets,
             b,
             w,
@@ -185,18 +190,16 @@ def step_2_compute(
             "token_api",
             steps,
             verbose=verbose,
-        )"""
-        import numpy as np
-
-        from portfolioqtopt.optimization.interpreter import Interpretation
-
-        interpretation = Interpretation(
-            selected_indexes=pd.Index(["A", "C", "D"], dtype="object"),
-            investments=np.array([0.75, 0.125, 0.125]),
-            expected_returns=44.5,
-            risk=17.153170260916784,
-            sharpe_ratio=2.594272622676201,
         )
+        # import numpy as np
+        # 
+        # interpretation = Interpretation(
+        #     selected_indexes=pd.Index(["A", "C", "D"], dtype="object"),
+        #     investments=np.array([0.75, 0.125, 0.125]),
+        #     expected_returns=44.5,
+        #     risk=17.153170260916784,
+        #     sharpe_ratio=2.594272622676201,
+        # )
     st.success("Hecho!", icon="✅")
     return interpretation
 
@@ -266,20 +269,30 @@ def app():
     logger.info(f"{' Enter App ':^50}")
     logger.info(f"{'-':->50}")
 
+    st.title("Portfolio Quantum Optimization")
+
+    with st.sidebar:
+        st.title("Parameter selection")
+        st.markdown(
+            "In this sidebar you are invited to parametrize the optimization process "
+            "in 3 ordered steps."
+        )
+        st.markdown("---")
+
     sub0, file_path, sheet_name = step_0_form()
 
     if sub0 or memory.get("step_1_form"):
 
         assert file_path is not None
 
-        st.markdown("## Loaded assets")
+        st.markdown("## 1. Historical assets")
 
         history = step_0_compute(file_path, sheet_name)
 
-        with st.expander("Raw historical assets"):
+        with st.expander("Raw data"):
             st.dataframe(data=history.df)
 
-        with st.expander("Plot the historical assets"):
+        with st.expander("Plot data"):
             visualize_assets(history)
 
         sub1, ns, er = step_1_form(history)
@@ -290,7 +303,7 @@ def app():
 
                 assert er is not None
 
-                st.markdown("## Assets simulation")
+                st.markdown("## 2. Assets simulation")
 
                 future = step_1_compute(history, ns, er, order=12)
 
@@ -304,7 +317,7 @@ def app():
 
                 if sub2:
 
-                    st.markdown("## Find the best portfolio")
+                    st.markdown("## 3. Find the best portfolio")
 
                     interpretation = step_2_compute(
                         future,
